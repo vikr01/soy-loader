666e5d0bd35394144e89e746783c24af7852ad60
vikr01 <vikr01@users.noreply.github.com>
2018-08-24 23:36:30 -0700

    added postlint-fix script

--- src/index.js
+++ src/index.js
@@ -16,86 +16,85 @@ const compileTemplateFilesAsync = promisify(soynode.compileTemplateFiles).bind(
 // Automatic cleanup of temporary files.
 
 // Run the loader.
-export default async function(source) {
-	const { cacheable, query } = this;
-
-	if (cacheable) this.cacheable();
-
+export default function(source) {
 	const loaderCallback = this.async();
 
-	const query2 =
-		query instanceof Object ? query : loaderUtils.parseQuery(this.query);
+	(async function load() {
+		const { cacheable, query } = this;
 
-	const { inputDir } = query2;
+		if (cacheable) this.cacheable();
 
-	let inputDir2;
+		const query2 =
+			query instanceof Object ? query : loaderUtils.parseQuery(this.query);
 
-	if (typeof inputDir === 'string' || inputDir) {
-		inputDir2 = `${inputDir}`;
-	} else if (!inputDir) {
-		inputDir2 = process.cwd();
-	}
+		const { inputDir } = query2;
 
-	const { classpath } = query2;
+		let inputDir2;
 
-	let classpath2;
+		if (typeof inputDir === 'string' || inputDir) {
+			inputDir2 = `${inputDir}`;
+		} else if (!inputDir) {
+			inputDir2 = process.cwd();
+		}
 
-	if (Array.isArray(classpath)) {
-		classpath2 = classpath;
-	} else if (classpath) {
-		classpath2 = [classpath];
-	} else {
-		classpath2 = [];
-	}
+		const { classpath } = query2;
 
-	const { pluginModules } = query2;
+		let classpath2;
 
-	let pluginModules2;
+		if (Array.isArray(classpath)) {
+			classpath2 = classpath;
+		} else if (classpath) {
+			classpath2 = [classpath];
+		} else {
+			classpath2 = [];
+		}
 
-	if (Array.isArray(classpath)) {
-		pluginModules2 = pluginModules;
-	} else if (classpath) {
-		pluginModules2 = [pluginModules];
-	} else {
-		pluginModules2 = [];
-	}
+		const { pluginModules } = query2;
 
-	// Get the configurable source of the soy runtime utilities, or use default.
-	const { utils } = query2;
+		let pluginModules2;
 
-	const runtimeUtilsPath = require.resolve(
-		utils || closureTemplates['soyutils.js']
-	);
-	// Create a require statement to be injected into the templates for shimming.
-	const runtimeUtilsStatement = `require('exports-loader?goog,soy,soydata,soyshim!${runtimeUtilsPath}')`;
-	const runtimeUtils = runtimeUtilsStatement.replace(/\\/g, '\\\\');
+		if (Array.isArray(classpath)) {
+			pluginModules2 = pluginModules;
+		} else if (classpath) {
+			pluginModules2 = [pluginModules];
+		} else {
+			pluginModules2 = [];
+		}
 
-	this.addDependency(require.resolve(closureTemplates['soyutils.js']));
-	soynode.setOptions({
-		inputDir: inputDir2,
-		outputDir: '/',
-		uniqueDir: false,
-		eraseTemporaryFiles: false,
-		classpath: classpath2,
-		pluginModules: pluginModules2,
-	});
+		// Get the configurable source of the soy runtime utilities, or use default.
+		const { utils } = query2;
 
-	// Grab namespace for shimming encapsulated module return value.
-	const extracted = /\{namespace\s+((\w+)[^\s]*).*\}/.exec(source);
-	const namespace = extracted[1];
-	const baseVar = extracted[2];
-	const tempDir = path.resolve(
-		__dirname,
-		[
-			'soytemp', // directory prefix
-			Date.now(), // datestamp
-			(Math.random() * 0x100000000 + 1).toString(36), // randomized suffix
-		].join('-')
-	);
+		const runtimeUtilsPath = require.resolve(
+			utils || closureTemplates['soyutils.js']
+		);
+		// Create a require statement to be injected into the templates for shimming.
+		const runtimeUtilsStatement = `require('exports-loader?goog,soy,soydata,soyshim!${runtimeUtilsPath}')`;
+		const runtimeUtils = runtimeUtilsStatement.replace(/\\/g, '\\\\');
 
-	// Compile the templates to a temporary directory for reading.
-	let template;
-	try {
+		this.addDependency(require.resolve(closureTemplates['soyutils.js']));
+		soynode.setOptions({
+			inputDir: inputDir2,
+			outputDir: '/',
+			uniqueDir: false,
+			eraseTemporaryFiles: false,
+			classpath: classpath2,
+			pluginModules: pluginModules2,
+		});
+
+		// Grab namespace for shimming encapsulated module return value.
+		const extracted = /\{namespace\s+((\w+)[^\s]*).*\}/.exec(source);
+		const namespace = extracted[1];
+		const baseVar = extracted[2];
+		const tempDir = path.resolve(
+			__dirname,
+			[
+				'soytemp', // directory prefix
+				Date.now(), // datestamp
+				(Math.random() * 0x100000000 + 1).toString(36), // randomized suffix
+			].join('-')
+		);
+
+		// Compile the templates to a temporary directory for reading.
 		await mkdirAsync(tempDir);
 		// Get the temp directory path
 		let dirPath = tempDir;
@@ -113,7 +112,7 @@ export default async function(source) {
 		await compileTemplateFilesAsync([soyPath]);
 
 		// Read the newly compiled source
-		template = await readFileAsync(`${path.resolve(soyPath)}.js`);
+		const template = await readFileAsync(`${path.resolve(soyPath)}.js`);
 		// Return utils and module return value, shimmed for module encapsulation.
 		loaderCallback(
 			null,
@@ -131,13 +130,9 @@ export default async function(source) {
 				`module.exports = ${namespace};`,
 			].join('\n')
 		);
-	} catch (err) {
-		// Handle any errors
-		// Cleanup temp directory
-		loaderCallback(err);
-	}
 
-	if (template) {
-		await promisify(rimraf)(tempDir);
-	}
+		if (template) {
+			await promisify(rimraf)(tempDir);
+		}
+	})().catch(err => loaderCallback(err));
 }
